ae31335e1b0522b8b865d71dc436610384b01d0c
Dan05Hacker <52347190+Dan05Hacker@users.noreply.github.com>
2023-12-02 19:02:22 +0800

    Add files via upload

---
+++ script.py
@@ -0,0 +1,68 @@
+# Arithmetics 
+# Addition
+print(4 +5)
+
+# Subtraction
+print(5-5)
+
+# Multiplication
+print(3*5)
+
+# Division
+print(10 / 2)
+
+# Calculations with variables
+# Create the variables monthly_savings and num_months
+monthly_savings = 10
+num_months = 4
+savings = 100
+
+# Multiply monthly_savings and num_months
+new_savings = monthly_savings * num_months
+
+# Add new_savings to your savings
+total_savings = new_savings + savings
+
+# Print total_savings
+print(total_savings)
+
+# Other variable types
+# Create a variable half
+half = 0.5
+
+# Create a variable intro
+intro = "Hello! How are you?"
+
+# Create a variable is_good
+is_good = True
+
+# Operations with other types
+monthly_savings = 10
+num_months = 12
+intro = "Hello! How are you?"
+
+# Calculate year_savings using monthly_savings and num_months
+year_savings = monthly_savings * num_months
+
+# Print the type of year_savings
+print(type(year_savings))
+
+# Assign sum of intro and intro to doubleintro
+doubleintro = intro + intro
+
+# Print out doubleintro
+print(doubleintro)
+
+#Type conversion
+# Definition of savings and total_savings
+savings = 100
+total_savings = 150
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(total_savings) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
